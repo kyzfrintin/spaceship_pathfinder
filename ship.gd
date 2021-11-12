d47f0756ba5276771a2dfd1075441160383890b7
Kyle Harmieson <kyz1610@gmail.com>
2021-11-12 06:00:32 +0000

    added second ship

--- ship.gd
+++ ship.gd
@@ -1,15 +1,20 @@
 extends Spatial
 
-onready var target = get_node("../target")
-
+export(NodePath) var target_dir
+export var speed := 0.4
 var target_pos : = Vector3.ZERO
 var movement : = Vector3.ZERO
+var target
 
+func _ready():
+	target = get_node(target_dir)
+	
 func _physics_process(delta):
+	if !target: return
 	target_pos = target.global_transform.origin
 	var dis = global_transform.origin.distance_to(target_pos)
-	if dis > 1:
+	if dis > 5:
 		var dir = $steering.calculate_path(target_pos)
-		movement = lerp(movement, dir*0.06,0.4)
+		movement = lerp(movement, dir*speed,0.4)
 		look_at(global_transform.origin+movement, global_transform.basis.y)
 		global_translate(movement)
